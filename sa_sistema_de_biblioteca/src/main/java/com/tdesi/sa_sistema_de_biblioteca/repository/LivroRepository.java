2e9d207b48d2f1290431553538b58d9e6a3d5f51
VINICIUS PEDRO ANDREAZZA <vinicius_andreazza@estudante.sesisenai.org.br>
2025-07-28 19:48:53 -0300

    Estrutura Inicial do Back-End

---
+++ sa_sistema_de_biblioteca/src/main/java/com/tdesi/sa_sistema_de_biblioteca/repository/LivroRepository.java
@@ -0,0 +1,12 @@
+package com.tdesi.sa_sistema_de_biblioteca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tdesi.sa_sistema_de_biblioteca.model.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    
+}
