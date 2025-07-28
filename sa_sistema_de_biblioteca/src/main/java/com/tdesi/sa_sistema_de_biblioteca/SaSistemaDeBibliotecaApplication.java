a45270ae8cc34423ae0c63b09bf26bf4ffddcbec
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2025-07-28 19:22:33 -0300

    criando estrutura do projeto

---
+++ sa_sistema_de_biblioteca/src/main/java/com/tdesi/sa_sistema_de_biblioteca/SaSistemaDeBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.tdesi.sa_sistema_de_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaSistemaDeBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaSistemaDeBibliotecaApplication.class, args);
+	}
+
+}
---
+++ sa_sistema_de_biblioteca/src/test/java/com/tdesi/sa_sistema_de_biblioteca/SaSistemaDeBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdesi.sa_sistema_de_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaSistemaDeBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
