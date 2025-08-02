f053f03a0fbaaaee0210796d67028d7e2307eb6b
William Colasso <william.colasso@gmail.com>
2025-08-02 16:33:45 -0300

    Cadastro de livro refeito JS+CSS

--- sa_sistema_de_biblioteca/src/main/java/com/tdesi/sa_sistema_de_biblioteca/controller/CategoriaLivroController.java
+++ sa_sistema_de_biblioteca/src/main/java/com/tdesi/sa_sistema_de_biblioteca/controller/CategoriaLivroController.java
@@ -1,12 +1,12 @@
 package com.tdesi.sa_sistema_de_biblioteca.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tdesi.sa_sistema_de_biblioteca.model.CategoriaLivro;
 
-@Controller
+@RestController
 @RequestMapping
 public class CategoriaLivroController {
     
